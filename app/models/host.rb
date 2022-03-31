409022e221da11d2f03e7953007c487dae23cf6c
Avery Miles <MilesA@chc.edu>
2022-03-31 08:22:10 -0400

    Continue working on Host and the Navbar

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,2 +1,14 @@
 class Host < ApplicationRecord
+	# has_many method specifies a one to many relationship
+	# appearances option specifies that one host has many appearances
+	# destroy causes all appearance to be destroyed when their
+	# related host is destroyed
+	has_many :appearances, dependent: :destroy
+	# movies option specifies that one actor has many host
+	# the through option specifies tht the relationship exists
+	# only through appearances
+	has_many :movies, through: :appearances
+	# validates method specifices that an actor is valid only if
+	# a first name and last name are present
+	validates :fullname, :email, presence: true
 end
